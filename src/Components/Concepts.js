c84818763ad88395df0d716c74b52062bbdb0ad1
dfurness <davidfurness@live.com>
2019-05-01 18:22:28 -0400

    Additions to the home page and concepts page

--- src/Components/Concepts.js
+++ src/Components/Concepts.js
@@ -25,8 +25,27 @@ class App extends Component {
         functional products. Sketches and storyboards are examples of prototypes that
         are used to illustrate a proposed to solution to a given problem. Paper
         prototypes of digital interfaces can be used to evaluate a potential user
-        interface before a single line of code is written.
+        interface before a single line of code is written. These can take the form of
+        low-fidelity, high-fidelity prototype or somwhere in the middle.
       </p>
+        <p className="p3">
+          Examples of low-fidelity:
+          <ul>
+            <li>Storyboards</li>
+            <li>Sketches</li>
+            <li>Paper interfaces</li>
+          </ul>
+        </p>
+
+        <p className="p2">
+        Examples of high-fidelity:
+          <ul>
+            <li>Functional Website</li>
+            <li>Functional Software(Alpha and Beta versions)</li>
+            <li>A device that closely resemble the final product</li>
+          </ul>
+        </p>
+
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,12 +12,12 @@ export default class Header extends React.Component {
       <h1>
         Prototyping
       </h1>
-        <div class="button-container">
-          <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("home")}>Home</button>
-          <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("concepts")}>Concepts</button>
-          <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("history")}>History</button>
-          <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("resources")}>Resources</button>
-        </div>
+      <div class="button-container">
+        <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("home")}>Home</button>
+        <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("concepts")}>Concepts</button>
+        <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("history")}>History</button>
+        <button class="button" style={{cursor: 'pointer'}} onClick={() => this.props.changePage("resources")}>Resources</button>
+      </div>
       </div>
     );
   }
